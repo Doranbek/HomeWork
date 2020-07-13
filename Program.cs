b48e2a60f1b0ac111f017d3f97139184326d87f7
doranbek <doronkg@mail.ru>
2020-07-13 22:53:02 +0600

    creat methods

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 
 namespace HomeWorkN8
 
@@ -11,59 +12,80 @@ namespace HomeWorkN8
         static void Main(string[] args)
         {
 
-
-            List<decimal> tempStat = new List<decimal>();
-            int numDay = 1;
-
-            while (true)
+            string exit;
+            do
             {
-                Console.Write($"Enter for  view statistics or set body temperature for {numDay}th day\n>");
-                var input = ((Console.ReadLine()));
-                if (input == "") break;
-                else if ((decimal.Parse(input) < 41) && (decimal.Parse(input) > 30))
+                Start();
+                InputBodyTemperatur();
+                Console.WriteLine("Your statistics daily body temperature");
+                OutputBodyTemperatur();
+                End();
+            } while (true);
+            
+
+            static void Start()
+            {
+                Console.WriteLine("Hello, the BodyTemperature program welcomes you!To start, press any key.");
+                Console.ReadKey();
+                Console.Clear();
+            }
+
+            static void InputBodyTemperatur()
+            {
+                List<decimal> tempStat = new List<decimal>();
+                int numDay = 1;
+
+                while (true)
                 {
-                    tempStat.Add(decimal.Parse(input));
+                    Console.Write($"Enter for  view statistics or set body temperature for {numDay}th day\n>");
+                    var input = ((Console.ReadLine()));
+                    if (input == "") break;
+                    else if ((decimal.Parse(input) < 41) && (decimal.Parse(input) > 30))
+                    {
+                        tempStat.Add(decimal.Parse(input));
+                        numDay++;
+                    }
+                    else Console.WriteLine("Body temperature incorrect");
+                }
+                Console.Clear();
+            
+            }
+            
+            static void OutputBodyTemperatur()
+            {
+                numDay = 1;
+                foreach (var i in tempStat)
+                {
+                    Console.WriteLine($"On {numDay}th day your body temperature was {i:0.0} °C");
                     numDay++;
                 }
-                else Console.WriteLine("Body temperature incorrect");
-            }
 
-
-            Console.WriteLine("Your statistics daily body temperature");
-            numDay = 1;
-            foreach (var i in tempStat)
-            {
-                Console.WriteLine($"On {numDay}th day your body temperature was {i:0.0} °C");
-                numDay++;
-            }
-
-            decimal min = tempStat.Min();
-            decimal max = tempStat.Max();
-            decimal avr = tempStat.Average();
+                decimal min = tempStat.Min();
+                decimal max = tempStat.Max();
+                decimal avr = tempStat.Average();
             
-            Console.WriteLine($"Your statistics daily body temperature Min:{min:0.0} Max:{max:0.0} Average{avr:0.0}");
-            //foreach (var dayCount in tempStat)
-            //{
-            //    NewMethod(tempStat, dayCount);
-            //}
+                Console.WriteLine($"Your statistics daily body temperature Min:{min:0.0} Max:{max:0.0} Average{avr:0.0}");
+                      
+                decimal normsize = (from i in tempStat where i <= (decimal)37.5 select i).Count();
+                Console.WriteLine($"You had a low body temperature {normsize} days");
 
-          
-            decimal normsize = (from i in tempStat where i <= (decimal)37.5 select i).Count();
-            Console.WriteLine($"You had a low body temperature {normsize} days");
+                decimal maxsize = (from i in tempStat where i >= (decimal)37.5 && i <= (decimal)38.3 select i).Count();
+                Console.WriteLine($"You had a high body temperature {maxsize} days");
 
-            decimal maxsize = (from i in tempStat where i >= (decimal)37.5 && i <= (decimal)38.3 select i).Count();
-            Console.WriteLine($"You had a high body temperature {maxsize} days");
-
-            decimal veryMaxsize = (from i in tempStat where i >= (decimal)38.3 select i).Count();
-            Console.WriteLine($"You had a very high body temperature {veryMaxsize} days");
-
+                decimal veryMaxsize = (from i in tempStat where i >= (decimal)38.3 select i).Count();
+                Console.WriteLine($"You had a very high body temperature {veryMaxsize} days");
+            }
+            
+            static string End()
+            {
+                Console.WriteLine("\nTo continue press any key \nTo exit the program press \"Q\"");
+                bool boolend=ToUpper(Console.ReadLine())=="Q";                
+                return boolend;
+            }
 
             Console.ReadKey();
         }
 
-        //private static void NewMethod(List<decimal> tempStat, decimal dayCount)
-        //{
-        //    dayCount.Where(dayCount => dayCount == tempStat.Min).Count();
-        //}
+        
     }
 }
